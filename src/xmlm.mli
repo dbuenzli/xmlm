7e615b67206ac31141381585ced734c3485caa60
Daniel Bünzli <daniel.buenzli@erratique.ch>
2008-03-17 10:17:16 +0000

    Made the examples compile and added them to test/examples.ml

--- src/xmlm.mli
+++ src/xmlm.mli
@@ -605,34 +605,33 @@ let ex_ns = (Xmlm.ns_xmlns, "ex"), "http://example.org/ex"]}
     input channel and outputs it.
 {[let id_seq ic oc = 
   let i = Xmlm.make_input (`Channel ic) in 
-  let o = Xmlm.make_output (`Channel oc) in 
+  let o = Xmlm.make_output ~nl:true (`Channel oc) in 
   while not (Xmlm.eoi i) do Xmlm.output o (Xmlm.input i) done]}
     The following function reads a {e sequence} of documents on the 
     input channel. In each document's tree it prunes non root elements
     whose name belongs to [prune_list].
 {[let prune_docs prune_list ic oc = 
   let i = Xmlm.make_input (`Channel ic) in
-  let o = Xmlm.make_output (`Channel oc) in
+  let o = Xmlm.make_output ~nl:true (`Channel oc) in
   let copy i o = Xmlm.output o (Xmlm.input i) in
   let prune (name, _) = List.mem name prune_list in
-  let process i o d = 
+  let rec process i o d = 
     let rec skip i d = match Xmlm.input i with
     | `El_start _ -> skip i (d + 1)
     | `El_end -> if d = 1 then () else skip i (d - 1)
-    | s -> skip i n
+    | s -> skip i d
     in
     match Xmlm.peek i with 
-    | `El_start tag when prune tag -> skip i 0; process i o l
-    | `El_start _ -> copy i o; process i o (l + 1)
-    | `El_end -> copy i o; if l = 1 then () else process i o (l - 1)
-    | `Data _ -> copy i o; process i o l
+    | `El_start tag when prune tag -> skip i 0; process i o d
+    | `El_start _ -> copy i o; process i o (d + 1)
+    | `El_end -> copy i o; if d = 0 then () else process i o (d - 1)
+    | `Data _ -> copy i o; process i o d
     | `Dtd _ -> assert false
   in
   let rec docs i o = 
     copy i o; (* `Dtd *)
     copy i o; (* root start *)
     process i o 0;
-    copy i o; (* root end *)
     if Xmlm.eoi i then () else docs i o
   in
   docs i o]}
@@ -645,7 +644,7 @@ let ex_ns = (Xmlm.ns_xmlns, "ex"), "http://example.org/ex"]}
     The following functions input/output xml documents from/to the abstractions 
     as value of type [tree].
 {[let in_tree i = 
-  let e tag childs = E (tags, childs)  in
+  let el tag childs = E (tag, childs)  in
   let data d = D d in
   Xmlm.input_doc_tree ~el ~data i
 
@@ -693,8 +692,8 @@ let out_tree o t =
   let tag n = ("", n), [] in
   let error () = invalid_arg "parse error" in
   let accept s i = if Xmlm.input i = s then () else error () in
-  let rec i_list el acc i = match Xmlm.peek i with 
-  | `El_start _ -> i_list el ((el i) :: acc) i
+  let rec i_seq el acc i = match Xmlm.peek i with 
+  | `El_start _ -> i_seq el ((el i) :: acc) i
   | `El_end -> List.rev acc
   | _ -> error ()
   in
@@ -710,49 +709,49 @@ let out_tree o t =
   in
   let i_bureaucrat i = 
     try
-      accept (`El_start (tag "bureaucrat"));
-      let name = i_el "name" in
-      let surname = i_el "surname" in
+      accept (`El_start (tag "bureaucrat")) i;
+      let name = i_el "name" i in
+      let surname = i_el "surname" i in
       let honest = match Xmlm.peek i with
-      | `El_start (("", "honest"), []) -> ignore (i_el "honest"); true
+      | `El_start (("", "honest"), []) -> ignore (i_el "honest" i); true
       | _ -> false
       in
-      let obf = float_of_string (i_el "obfuscation_level") in
-      let trs = i_list (i_el "tr") i in
+      let obf = float_of_string (i_el "obfuscation_level" i) in
+      let trs = i_seq (i_el "tr") [] i in
       accept (`El_end) i;
       { name = name; surname = surname; honest = honest; 
-	obfuscation_level = obf; trs = trs }
+        obfuscation_level = obf; trs = trs }
     with
     | Failure _ -> error () (* float_of_string *)
   in
   accept (`Dtd None) i;
   accept (`El_start (tag "list")) i;
-  let bl = i_list i_bureaucrat [] i in
+  let bl = i_seq i_bureaucrat [] i in
   accept (`El_end) i;
   if not (Xmlm.eoi i) then invalid_arg "more than one document";
   bl
 
 let out_w3c_bureaucrats dst bl = 
   let tag n = ("", n), [] in
-  let o = Xmlm.make_output dst in
+  let o = Xmlm.make_output ~nl:true ~indent:(Some 2) dst in
   let out = Xmlm.output o in
   let o_el n d = 
     out (`El_start (tag n)); 
-     if d <> "" then out (`Data d); 
+    if d <> "" then out (`Data d); 
     out `El_end 
   in
   let o_bureaucrat b = 
-    out `El_start (tag "bureaucrat");
-     o_el "name" b.name;
-     o_el "surname" b.surname;
-     if b.honest then o_el "honest" "";
-     o_el "obfuscation_level" (string_of_float b.obfuscation_level);
-     List.iter (o_el "tr") b.trs;
+    out (`El_start (tag "bureaucrat"));
+    o_el "name" b.name;
+    o_el "surname" b.surname;
+    if b.honest then o_el "honest" "";
+    o_el "obfuscation_level" (string_of_float b.obfuscation_level);
+    List.iter (o_el "tr") b.trs;
     out `El_end
   in
   out (`Dtd None);
   out (`El_start (tag "list"));
-   List.iter o_bureaucrat bl;
+  List.iter o_bureaucrat bl;
   out (`El_end)]}
 *)
 
