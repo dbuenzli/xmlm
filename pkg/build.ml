ee7e06df647a1554677a3964eda238f3564394bb
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-10-01 16:51:08 +0200

    Use topkg.

---
+++ pkg/build.ml
@@ -0,0 +1,14 @@
+#!/usr/bin/env ocaml
+#directory "pkg";;
+#use "topkg.ml";;
+
+let () =
+  Pkg.describe "xmlm" ~builder:`OCamlbuild [
+    Pkg.lib "pkg/META";
+    Pkg.lib ~exts:Exts.module_library "src/xmlm";
+    Pkg.bin ~auto:true "test/xmltrip";
+    Pkg.doc "README.md";
+    Pkg.doc "CHANGES.md";
+    Pkg.doc "test/examples.ml";
+    Pkg.doc "test/xhtml.ml";
+  ]
