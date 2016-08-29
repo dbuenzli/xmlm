e11197fc2ec0d8b8d5fb36d0215fdd6177bf3be2
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-08-29 14:55:53 +0200

    Simplify grammar of signals. Thanks to Frederic Bour for the def.

--- src/xmlm.mli
+++ src/xmlm.mli
@@ -65,9 +65,10 @@ type signal = [ `Dtd of dtd | `El_start of tag | `El_end | `Data of string ]
     to the language of the [doc] grammar :
     {[doc ::= `Dtd tree
 tree ::= `El_start child `El_end
-child ::= `Data | tree | child tree | tree child tree | tree child | epsilon ]}
-    The choices for [child] expresses the fact that there will never be
-    two consecutive `Data signals in the children of an element.
+child ::= `Data trees | trees
+trees ::= tree child | epsilon]}
+    The [trees] production is used to expresses the fact that there will
+    never be two consecutive `Data signals in the children of an element.
 
     Input and output deal only with well-formed sequences or
     exceptions are raised. However on output consecutive [`Data]
