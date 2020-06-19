5da0fa6000268356178202724a4068534c37d7ae
Daniel Bünzli <daniel.buenzli@erratique.ch>
2020-06-19 17:54:08 +0200

    Remove a few watermarks.

--- src/xmlm.mli
+++ src/xmlm.mli
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2007 Daniel C. Bünzli. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 (** Streaming XML codec.
